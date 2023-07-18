cf057dc98a75137f54f5f81708d272984e797559
Dancloud <66711049+Danny-C-Auditore@users.noreply.github.com>
2023-07-19 01:16:28 +0800

    Update HFtest.py

--- HFtest.py
+++ HFtest.py
@@ -16,7 +16,7 @@ If a question does not make any sense, or is not factually coherent, explain why
 # set paramenters
 parser = argparse.ArgumentParser()
 parser.add_argument('--dir', default='.\\data')
-parser.add_argument('--Q',default="What did the author do growing up?")
+parser.add_argument('--Q',default=None)
 args = parser.parse_args()
 dir = args.dir
 Q = args.Q
@@ -33,11 +33,11 @@ llm = HuggingFaceLLM(
     generate_kwargs={"temperature": 0.7, "do_sample": False},
     system_prompt=system_prompt,
     query_wrapper_prompt=query_wrapper_prompt,
-    tokenizer_name="StabilityAI/stablelm-tuned-alpha-3b",
-    model_name="StabilityAI/stablelm-tuned-alpha-3b",
+    tokenizer_name="openlm-research/open_llama_13b",
+    model_name="openlm-research/open_llama_13b",
     device_map="auto",
-    stopping_ids=[50278, 50279, 50277, 1, 0],
-    tokenizer_kwargs={"max_length": 4096},
+    #stopping_ids=[50278, 50279, 50277, 1, 0],
+    tokenizer_kwargs={"max_length": 2048},
     # uncomment this if using CUDA to reduce memory usage
     model_kwargs={"torch_dtype": torch.float16}
 )
