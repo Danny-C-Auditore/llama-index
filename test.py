911c738e76cb1b162f73c8bef21186ddf7159646
Dancloud <66711049+Danny-C-Auditore@users.noreply.github.com>
2023-07-18 18:49:03 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+
 import torch
 from transformers import pipeline
 from typing import Optional, List, Mapping, Any
@@ -10,14 +11,9 @@ from llama_index import (
 )
 from llama_index.llms import CustomLLM, CompletionResponse, LLMMetadata, CompletionResponseGen
 
-# set context window size
-context_window = 2048
-#set number of output tokens
-num_output = 256
+import argparse
 
-# store the pipeline or model outside of the LLM class to aovid memory issue
-model_name = "csitfun/llama-7b-logicot"
-pipeline = pipeline("text-generation", model=model_name, device="cuda:0", model_kwargs={"torch_dtype":torch.bfloat16})
+
 
 class OurLLM(CustomLLM):
 
@@ -40,17 +36,42 @@ class OurLLM(CustomLLM):
         raise NotImplementedError()
 
 
-# define our own LLM
-llm = OurLLM()
+if __name__ == '__main__':
 
-service_context = ServiceContext.from_defaults(
-    llm=llm, context_window=context_window, num_output=num_output
-)
+    # set context window size
+    context_window = 2048
+    # set number of output tokens
+    num_output = 256
 
-doucuments = SimpleDirectoryReader("paul_graham_essay").load_data()
-index = ListIndex.from_documents(doucuments, service_context=service_context)
 
-# Query and print response
-query_engine = index.as_query_engine()
-response = query_engine.query("Why did the author's advisor say nothing about the strange class?")
-print(response)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--directory', default=None)
+    parser.add_argument('--prompt', default=None)
+    parser.add_argument('--m', default="openlm-research/open_llama_3b")
+    parser.add_argument('--device',default="cuda:0")
+    args = parser.parse_args()
+    dir = args.directory
+    query = args.prompt
+    device = args.device
+
+    # store the pipeline or model outside of the LLM class to aovid memory issue
+    model_name = args.model
+    pipeline = pipeline("text-generation", model=model_name, device=device,
+                        model_kwargs={"torch_dtype": torch.bfloat16})
+    # define our own LLM
+    llm = OurLLM()
+
+    service_context = ServiceContext.from_defaults(
+        llm=llm, context_window=context_window, num_output=num_output
+    )
+
+    # Load the data
+    doucuments = SimpleDirectoryReader(input_dir=dir).load_data()
+    index = ListIndex.from_documents(doucuments, service_context=service_context)
+
+    # Query and print response
+    query_engine = index.as_query_engine()
+    response = query_engine.query(query)
+    print(response)
+
