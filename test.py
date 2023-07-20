ea178afb290362c56ebd7e77162a9f5c20f4d18a
Dancloud <66711049+Danny-C-Auditore@users.noreply.github.com>
2023-07-20 14:38:45 +0800

    Update test.py

--- test.py
+++ test.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
     # set context window size
     context_window = 2048
     # set number of output tokens
-    num_output = 256
+    num_output = 1024
 
     os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:32"
 
@@ -74,11 +74,11 @@ if __name__ == '__main__':
     # Load the data
     documents = SimpleDirectoryReader(input_dir=dir).load_data()
     #documents = SimpleDirectoryReader(input_files=[file]).load_data()
-    print(documents)
+    #print(documents)
     index = ListIndex.from_documents(documents, service_context=service_context)
 
     # Query and print response
     query_engine = index.as_query_engine()
-
+    print("##################### response is below ######################")
     response = query_engine.query(query)
     print(response)
